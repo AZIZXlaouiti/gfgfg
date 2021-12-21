46cb1dd29c0b11c83a96ebb4add4969c33786123
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-20 20:39:10 -0500

    addingg mikro orm

---
+++ src/entities/Post.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+// adding schema 
+@Entity()
+export class Post {
+
+  @PrimaryKey()
+  _id!: number;
+
+  @Property()
+  createdAt: Date = new Date();
+
+  @Property({ onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  @Property()
+  title!: string;
+
+
+//   @OneToMany(() => Book, book => book.author)
+//   books = new Collection<Book>(this);
+
+//   @ManyToMany(() => Author)
+//   friends = new Collection<Author>(this);
+
+//   @ManyToOne(() => Book, { nullable: true })
+//   favouriteBook?: Book;
+
+ 
+
+//   constructor(name: string, email: string) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("compiling to ts")
+import {MikroORM} from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async ()=>{
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'redditdb',
+        type:"postgresql",
+        debug: !__prod__,
+        user:'postgres',
+        password:'postgres',
+        port:5432,
+        host:'localhost',
+    });
+
+    await orm.isConnected();
+    orm.em.create(Post , {title: 'first time using mikroOrm'})
+};
+main()
