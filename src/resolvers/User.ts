996cfc98ab36e01e4c49b342a2c0e4d4d389fef2
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-22 18:37:52 -0500

    adding user queries

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,14 +1,22 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Ctx, Field, Resolver, Arg, Mutation, InputType } from "type-graphql";
+import { ObjectType,Ctx, Field, Resolver, Arg, Mutation, InputType , Query } from "type-graphql";
 import argon2 from 'argon2'
-@InputType()
+@InputType()// used in Arg
 class UsernamePasswordInput {
     @Field()
     username: string
     @Field()
     password: string
 
+}
+
+@ObjectType() // returned from mutation decorator 
+class UserResponse {
+  @Field(()=> [Error] , {nullable: true})
+  errors?: Error[]
+  @Field(()=> User, {nullable: true})
+  user?: User
 }
 @Resolver()
 export class UserResolver {
@@ -20,7 +28,29 @@ export class UserResolver {
         const hash = await argon2.hash(options.password);
         const user = em.create(User, { username: options.username, password: hash });
         await em.persistAndFlush(user);
-        return User;
+        return user
     }
+    @Query(()=> [User])
+    async users(
+        @Ctx() {em}:MyContext
+    ){
+        return em.find(User , {})
+     }
+    @Mutation(() => User)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ) {
+        try{
 
+            const user = em.findOne(User , {username: options.username})
+        }
+        catch{
+            return {
+                errors: [{
+
+                }]
+            }
+        }
+    }
 }
