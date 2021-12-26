d1774d840cdf46b08ad31c5e0113d4ca48f411a4
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-26 18:14:26 -0500

    adding me Query

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -26,6 +26,20 @@ class UserResponse {
 }
 @Resolver()
 export class UserResolver {
+    @Query (()=> User , {nullable: true})
+    async me(
+         @Ctx() { req , em }:MyContext
+     ){
+       if (!req.session.userId){
+           return null;
+       }
+       
+       const user = await em.findOne(User , {_id: req.session.userId})
+       return user;
+     }
+     
+
+
     @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
