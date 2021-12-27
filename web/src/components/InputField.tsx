4008dab4b0c0e9bcde38ed82a0326c5d2db5da02
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-27 14:43:17 -0500

    small name changes

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -1,13 +1,13 @@
-import { FormControl, FormLabel, FormErrorMessage, usePinInputField } from '@chakra-ui/react';
+import { FormControl, FormLabel, FormErrorMessage, usePinInputField  , Input} from '@chakra-ui/react';
 import React, { InputHTMLAttributes } from 'react'
 import{ useField } from  'formik'
 type InputProps  = InputHTMLAttributes<HTMLInputElement> & {
     name: string // name is required with this type declaration
-    label?: string 
+    label: string 
     placeholder: string
 }
 
-export const Input: React.FC<InputProps> = (props) => {
+export const InputField: React.FC<InputProps> = (props) => {
     const [field , meta , helpers] = useField(props)
         return (
             <FormControl isInvalid={!!meta.error}>
