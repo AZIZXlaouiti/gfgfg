a2a6b73c8e671e251811f310a8d55d8109f71b6c
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-27 00:56:40 -0500

    ui cleanup

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -2,8 +2,8 @@ import { Flex, Heading } from '@chakra-ui/react'
 
 export const Hero = ({ title }: { title: string }) => (
   <Flex
-    justifyContent="center"
-    alignItems="center"
+    // justifyContent="center"
+    // alignItems="center"
     height="100vh"
     bgGradient="linear(to-l, #7928CA, #FF0080)"
     bgClip="text"
@@ -13,5 +13,5 @@ export const Hero = ({ title }: { title: string }) => (
 )
 
 Hero.defaultProps = {
-  title: 'with-chakra-ui-typescript',
+  title: 'graphQl typescript',
 }
