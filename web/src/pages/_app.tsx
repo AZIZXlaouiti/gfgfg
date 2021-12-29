1cf006620dcbb36f6e32c144099225aa5321a9f9
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-29 15:34:21 -0500

    adding urql caching

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
 import { AppProps } from 'next/app'
-import { Provider , createClient } from 'urql'
+import { Provider , createClient, dedupExchange, fetchExchange } from 'urql'
 import "../styles/style.css"
-
+import { cacheExchange } from '@urql/exchange-graphcache';
 function MyApp({ Component, pageProps }: AppProps) {
+  // creating urql client 
   const client = createClient({
     url: 'http://localhost:8000/graphql',
     fetchOptions:{
         credentials: "include",  // allowing server to send cookie
-    }
+    },
+    exchanges: [dedupExchange, cacheExchange({}), fetchExchange],
   });
   return (
     <Provider
