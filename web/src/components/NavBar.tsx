1acb89ccee506e45f9b9fc3287d32cd05544271a
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-29 14:53:50 -0500

    small changes

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,45 +1,58 @@
-import { Box, Button  , Link as ChakraLink} from '@chakra-ui/react';
+import { Box, Button, Link as ChakraLink } from '@chakra-ui/react';
 import React from 'react'
 import { Container } from './Container';
 import NextLink from 'next/link'
+import { useCurrentUserQuery } from '../generated/graphql';
 
 interface NavBarProps {
 
 }
 
-export const NavBar: React.FC<NavBarProps> = ({}) => {
-        return (
-           <Box 
-             position="fixed"
-             top="1rem"
-             left="1rem"
-             color="green"
-           >
-               
+export const NavBar: React.FC<NavBarProps> = ({ }) => {
+    const [{ data, fetching }] = useCurrentUserQuery()
+ 
+    return (
+        <Box
+            position="fixed"
+            top="1rem"
+            left="1rem"
+            color="green"
+        >
             <Container
                 flexDirection="row"
                 position="fixed"
                 width="100%"
                 maxWidth="15rem"
                 py={3}
-                >
-                  <ChakraLink
-                    flexGrow={3}
-                    mx={2}
-                    href='/login'
-                    >
-                <Button width="100%" variant="solid" colorScheme="green">
-                    login
-                </Button>
-                </ChakraLink>
+            >  {
+                fetching? null:!data?.me ?
+                    <>
+                        <ChakraLink
+                            flexGrow={3}
+                            mx={2}
+                            href='/login'
+                        >
+                            <Button width="100%" variant="solid" colorScheme="green">
+                                login
+                            </Button>
+                        </ChakraLink>
 
-              <ChakraLink  href='/register' flexGrow={1} mx={2}>
-               <Button width="100%" variant="outline" colorScheme="green">
-                sign up
-               </Button>
-             </ChakraLink>
-             </Container>
-                    </Box>
-        );
+                        <ChakraLink href='/register' flexGrow={1} mx={2}>
+                            <Button width="100%" variant="outline" colorScheme="green">
+                                sign up
+                            </Button>
+                        </ChakraLink>
+                    </> : <ChakraLink flexGrow={1} mx={2}>
+                        <Button width="100%" variant="outline" colorScheme="green">
+                            {data.me.username}
+                        </Button>
+                    </ChakraLink>
+                
+            }
+                
+            </Container>
+
+        </Box>
+    );
 }
 // isExternal --> open new tab  <<ChakraLink>>
